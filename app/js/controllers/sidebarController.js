516eadb108e46d7eb7d3d466a01a713d2d103374
P-ppc <ppc-user@foxmail.com>
2016-11-18 22:57:17 +0800

    添加sidebar.histroy和sidebar.project

---
+++ app/js/controllers/sidebarController.js
@@ -0,0 +1,28 @@
+AppModule.controller('sidebarController', function ($scope) {
+    var URL = {
+        history: "/static/tpl/sidebar.history.html",
+        project:  "/static/tpl/sidebar.project.html"
+    };
+    $scope.sidebar = {
+        url: URL.project,
+        tab: {
+            history: 'HISTORY',
+            project: 'PROJECT',
+        },
+        class: {
+            history: "",
+            project: "selected"
+        }
+    };
+    $scope.switchTab = function (tabName) {
+        if (tabName === $scope.sidebar.tab.history) {
+            $scope.sidebar.url = URL.history;
+            $scope.sidebar.class.history = "selected";
+            $scope.sidebar.class.project = "";
+        } else if (tabName === $scope.sidebar.tab.project) {
+            $scope.sidebar.url = URL.project;
+            $scope.sidebar.class.history = "";
+            $scope.sidebar.class.project = "selected";
+        }
+    };
+});
